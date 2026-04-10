b53d8940e0dcae10205e7148ea6f88d29002da46
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-04-10 12:56:55 -0300

    comentando a integração

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:3000',
-    credentials: true,
+    credentials: true, //passo 1 da integração: o CORS foi habilitado aqui no main.ts
   });
 
   app.useGlobalPipes(
