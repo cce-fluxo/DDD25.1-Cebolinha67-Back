0fc2f7076d3861ef2abc77b9ac986463205fa51b
pedronvmotta <pedronaves.20241@poli.ufrj.br>
2026-05-21 19:09:49 -0300

    consertando problemas pequenos

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:8081', 'http://192.168.68.109:8081'],
+    origin: ['http://localhost:3000', 'http://localhost:8081', 'http://192.168.68.109:8081', 'http://192.168.15.10:8081'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
